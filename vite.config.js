a0c215df90a6cbb3eeaaadb4d1606920e5730229
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-09-19 22:37:41 +0300

    add files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: { sourcemap: true },
+  resolve: {
+    alias: {
+      components: "/src/components",
+      data: "/src/data",
+      helpers: "/src/helpers",
+      styles: "/src/styles",
+    },
+  },
+});
