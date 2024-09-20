2bdb088e30d46832d5440c649bb4d99b3f2654c4
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-09-20 19:27:02 +0300

    add frienlist

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Profile from './components/Profile/Profile';
 // import FriendList from './FriendList/FriendList';
 
 import userData from "./components/data/userData.json";
+import friends from "./components/data/friends.json";
 
 export default function App() {
   return (
