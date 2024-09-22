c20115fbee5c75b57a46b90caf0ce245ff2c91d3
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-09-22 12:58:07 +0300

    add historyTrans

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import Profile from './components/Profile/Profile';
-import FriendList from './components/FriendList/FriendList';
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 import userData from "./components/data/userData.json";
 import friends from "./components/data/friends.json";
+import transactions from "./components/data/transactions.json";
 
 export default function App() {
   return (
     <>
-			<Profile
-				name={userData.username}
-				tag={userData.tag}
-				location={userData.location}
-				image={userData.avatar}
-				stats={userData.stats}
-			/>
-			<FriendList friends={friends} />
-		</>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 }
